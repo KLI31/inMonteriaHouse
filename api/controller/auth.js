b213528b8f8875ce1eb776f6665aaff450ae7515
Luis David <lrambao5432@gmail.com>
2024-02-15 23:54:29 -0500

    :construction: Create controllers

---
+++ api/controller/auth.js
@@ -0,0 +1,19 @@
+import User from "../models/user.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json("Usuario creado con exito!");
+  } catch (error) {
+    next(errorHandler(500, "error desde la funcion"));
+  }
+};
