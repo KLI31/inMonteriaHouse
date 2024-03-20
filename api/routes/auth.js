18b9d8ea8a43f8337c46d5a679205c22544ab5e1
Luis David <lrambao5432@gmail.com>
2024-03-19 22:51:21 -0500

    :recycle: Create and modify routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { signup } from "../controller/auth.js";
+import { signin, signup, google, signOutUser } from "../controller/auth.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-
+router.post("/signin", signin);
+router.post("/google", google);
+router.get("/signout", signOutUser);
 export default router;
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { test } from "../controller/user.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  getUserListings,
+  getUser,
+} from "../controller/user.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/test", test);
-
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 export default router;
