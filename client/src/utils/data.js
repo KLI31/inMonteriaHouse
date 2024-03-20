c4c2d6ec69373e6e61eb843662d6ad6ffa33b645
Luis David <lrambao5432@gmail.com>
2024-03-19 23:19:39 -0500

    :recycle: Create data to comments

---
+++ client/src/utils/data.js
@@ -0,0 +1,56 @@
+export const Testimonials = [
+  {
+    name: "Juan Pérez",
+    image:
+      "https://images.unsplash.com/photo-1564564244660-5d73c057f2d2?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    occupation: "Programador",
+    description:
+      "¡Gracias a esta increíble aplicación de bienes raíces, pude encontrar la casa de mis sueños! Es fácil de usar y tiene una amplia selección de propiedades. ¡Altamente recomendada!",
+    rating: 5,
+  },
+  {
+    name: "María García",
+    image:
+      "https://images.unsplash.com/photo-1581182800629-7d90925ad072?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    occupation: "Ama de casa",
+    description:
+      "Esta aplicación de bienes raíces es simplemente fantástica. Me ayudó a encontrar mi primer apartamento de forma rápida y sencilla. ¡No puedo recomendarla lo suficiente!",
+    rating: 5,
+  },
+  {
+    name: "Luis Martínez",
+    image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FyYSUyMGRlJTIwaG9tYnJlfGVufDB8fDB8fHww",
+    occupation: "Obrero",
+    description:
+      "¡Qué aplicación de bienes raíces tan asombrosa! Me ayudó a encontrar un lugar perfecto para mi familia. La interfaz es intuitiva y la variedad de propiedades es impresionante.",
+    rating: 3,
+  },
+  {
+    name: "Ana López",
+    image:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    occupation: "abogada",
+    description:
+      "Esta aplicación de bienes raíces ha sido una bendición para mí. Encontré mi nuevo hogar en cuestión de días gracias a su interfaz fácil de usar y su amplia gama de opciones.",
+    rating: 4,
+  },
+  {
+    name: "Pedro Rodríguez",
+    image:
+      "https://images.unsplash.com/photo-1618077360395-f3068be8e001?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    occupation: "Ingeniero Civil",
+    description:
+      "¡No puedo decir suficientes cosas buenas sobre esta aplicación de bienes raíces! Me ayudó a encontrar la casa perfecta para mi familia de manera rápida y eficiente. ¡Gracias!",
+    rating: 5,
+  },
+  {
+    name: "Elena Fernández",
+    image:
+      "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    occupation: "Diseñadora UX/UI",
+    description:
+      "Como diseñadora, puedo decir que esta aplicación de bienes raíces tiene una interfaz muy bien diseñada. Es intuitiva y fácil de usar, lo que la hace perfecta para encontrar tu próximo hogar.",
+    rating: 5,
+  },
+];
